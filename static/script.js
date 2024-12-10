6837ed11f7b3afb37f3a0f339c52df0d7c4ec9c1
Ronit <83586482+RonitPatil@users.noreply.github.com>
2024-12-10 10:49:11 -0700

    Update script.js

--- static/script.js
+++ static/script.js
@@ -20,7 +20,7 @@ async function searchBooks() {
 
 
     try {
-        const response = await fetch(`https://book-search-self.vercel.app/search?q=${encodeURIComponent(query)}`);
+        const response = await fetch(`https://https://book-search-tau.vercel.app/search?q=${encodeURIComponent(query)}`);
         const data = await response.json();
 
         loadingDiv.style.display = 'none';
